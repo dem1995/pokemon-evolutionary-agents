a7c92a65c63fc65fb9574011be1c17051e38d9de
DEMcKnight <dem@ou.edu>
2020-03-29 14:41:31 -0600

    Cleaning up TypePlayer a bit

--- src/agents/typeplayer.py
+++ src/agents/typeplayer.py
@@ -5,17 +5,12 @@ class TypePlayer(Player):
 	def choose_move(self, battle: Battle):
 		# If the player can attack, it will
 
-		print(battle.active_pokemon.base_stats)
 		if battle.available_moves:
 			# Finds the most type-effective move among available ones
 			for move in battle.available_moves:
 				opptypes = battle.opponent_active_pokemon.types
 				if move.type.damage_multiplier(*battle.opponent_active_pokemon.types)>1:
 					return self.create_order(move)
-		# if battle.available_moves:
-		# 	# Finds the best move among available ones
-		# 	best_move = max(battle.available_moves, key=lambda move: move.base_power)
-		# 	return self.create_order(best_move)
 
-		# If no attack is available, a random switch will be made
+		# If no type-advantageous attack is chosen, a random move will be chosen.
 		return self.choose_random_move(battle)
