12381d911cf68096614918935a763de38373044a
DEMcKnight <dem@ou.edu>
2020-04-11 08:17:05 -0500

    Finalizing the Toggle Categories for preliminary testing

--- src/DSL/dsl_toggle_categories.py
+++ src/DSL/dsl_toggle_categories.py
@@ -4,12 +4,11 @@ from enum import Flag, auto
 class TOGGLE_CATEGORY(Flag):
 	"""Flags for determining which DSL methods to include"""
 	MANUAL_STATCHECK = auto()
-	MANUAL_MOVECHECK = auto()
 	AUTO_STATCHECK = auto()
-	AUTO_MOVECHECK = auto()
+	MISC_MOVECHECKS = auto()
 	CORE = auto()
-	BASIC = MANUAL_STATCHECK | CORE
-	ADVANCED = AUTO_STATCHECK | CORE
+	BASIC = MANUAL_STATCHECK | CORE | MISC_MOVECHECKS
+	ADVANCED = AUTO_STATCHECK | CORE | MISC_MOVECHECKS
 
 def decorate_toggle_category(togglecategory: TOGGLE_CATEGORY):
 	"""Decorator that attaches a toggle category to a method"""
